e2f0adff7ab6345031f323fef0c7df02f8fe1a5d
winatabayu <winatabayu01@gmail.com>
2024-08-10 01:30:25 +0700

    feat: adding illuminate blade on composer

---
+++ src/cache/85a71f78221bebf263be33eb638341ac.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>My View</title>
+</head>
+<body>
+<h1>Hello, <?php echo e($data); ?></h1>
+</body>
+</html><?php /**PATH /Users/winatabayu/PhpstormProjects/php-chisu-integration/src/views/welcome.blade.php ENDPATH**/ ?>
